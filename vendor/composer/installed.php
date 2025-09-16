0fe04c4140af36be5d9cea5c806538b6492fe236
ralfcarlo123 <ralfcarlolegaspi@gmail.com>
2025-09-16 13:13:33 +0800

    dock

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '4436811ccfdeee59b4e6d46886a8a98ccaab7e02',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '4436811ccfdeee59b4e6d46886a8a98ccaab7e02',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
